ca938ed01922dc8bfed6e5b8e9f41bc240917466
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-05-04 23:41:46 +0200

    create aspect : setup pointcut declarations

--- src/main/java/com/gemalto/aspect/CRMLoggingAspect.java
+++ src/main/java/com/gemalto/aspect/CRMLoggingAspect.java
@@ -1,6 +1,7 @@
 package com.gemalto.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
@@ -12,7 +13,25 @@ public class CRMLoggingAspect {
     // setup logger
     private Logger logger = Logger.getLogger(getClass().getName());
 
-    // setup poincut declarations
+    // setup poincut declarations for controller package
+    @Pointcut("execution(* com.gemalto.controllers.*.*(..))")
+    private void forControllerPackage() {
+    }
+
+    // setup poincut declarations for service package
+    @Pointcut("execution(* com.gemalto.service.*.*(..))")
+    private void forServicePackage() {
+    }
+
+    // setup poincut declarations for dao package
+    @Pointcut("execution(* com.gemalto.dao.*.*(..))")
+    private void forDaoPackage() {
+    }
+
+    // combine poincuts expressions
+    @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+    private void forAppFlow() {
+    }
 
     // add @Before advice
 
